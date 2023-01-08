bfb5fbf5b984d8843a89bc062d55678c7579af41
Patrick Smith <pas3n7@gmail.com>
2023-01-08 14:47:59 -0600

    Just comment out the beeps in the counter_face for now

--- movement/watch_faces/complication/counter_face.c
+++ movement/watch_faces/complication/counter_face.c
@@ -59,7 +59,7 @@ bool counter_face_loop(movement_event_t event, movement_settings_t *settings, vo
                 state->counter_idx=0;//reset counter index
             }
             print_counter(state);
-            beep_counter(state);
+            // beep_counter(state);
             break;
         case EVENT_ALARM_LONG_PRESS:
             state->counter_idx=0; // reset counter index
